9cb6bc7f6d273b1e7cc0f08acfc2993a388c744a
Your Name <you@example.com>
2023-04-19 22:18:22 +0530

    Updated file structure

--- clientside/src/main.jsx
+++ clientside/src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {BrowserRouter as Router} from 'react-router-dom'
-import { ChainId,ThirdwebProvider } from "@thirdweb-dev/react";
+import { ChainId,ThirdwebAuthProvider,ThirdwebProvider } from "@thirdweb-dev/react";
 import  App  from "./App";
 const root=ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ThirdwebProvider desiredChainId={ChainId.Sepolia}>
+  <ThirdwebAuthProvider desiredChainId={ChainId.Sepolia}>
     <Router>
       <App/>
     </Router>
 
-  </ThirdwebProvider>
+  </ThirdwebAuthProvider>
 )
---
+++ clientside/src/utils/index.js
@@ -0,0 +1,27 @@
+// Functions that we reuse often....
+
+
+
+export const daysLeft = (deadline) => {
+    const difference = new Date(deadline).getTime() - Date.now();
+    const remainingDays = difference / (1000 * 3600 * 24);
+  
+    return remainingDays.toFixed(0);
+  };
+
+//   To calculate the amount of money raised w.r.t to the total amount
+  export const calculateBarPercentage = (goal, raisedAmount) => {
+    const percentage = Math.round((raisedAmount * 100) / goal);
+  
+    return percentage;
+  };
+  
+  export const checkIfImage = (url, callback) => {
+    const img = new Image();
+    img.src = url;
+  
+    if (img.complete) callback(true);
+  
+    img.onload = () => callback(true);
+    img.onerror = () => callback(false);
+  };
