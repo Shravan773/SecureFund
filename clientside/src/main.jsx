b3a253f10925d3298664ce868c0e19c30077ca8f
Your Name <you@example.com>
2023-04-19 21:46:35 +0530

    Working on inital frontend

--- clientside/src/main.jsx
+++ clientside/src/main.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import { ReactDOM } from 'react-dom/client';
-import { BrowserRouter as Router } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {BrowserRouter as Router} from 'react-router-dom'
 import { ChainId,ThirdwebProvider } from "@thirdweb-dev/react";
-import { App } from "./App";
-
+import  App  from "./App";
 const root=ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <ThirdwebProvider desiredChainId={ChainId.Sepolia}>
-        <Router>
-            <App/>
-            
-        </Router>
 
-    </ThirdwebProvider>
+root.render(
+  <ThirdwebProvider desiredChainId={ChainId.Sepolia}>
+    <Router>
+      <App/>
+    </Router>
+
+  </ThirdwebProvider>
 )
